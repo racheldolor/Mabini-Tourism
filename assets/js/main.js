d26673db389375a496cf85a58c3bcd46fcbcc14d
racheldolor <22-43307@batstate-u.edu.ph>
2026-04-23 10:36:42 +0800

    feat: replaced carousel images and background

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,7 +81,7 @@ function formatItinerary(text) {
 }
 
 //background image slider
-const sliderImage = ["assets/images/bg.jpg", "assets/images/5.jpg", "assets/images/1.jpg", "assets/images/3.jpg", "assets/images/2.jpg", "assets/images/4.jpg", "assets/images/6.jpg"];
+const sliderImage = ["assets/images/bg.jpg", "assets/images/5.jpg", "assets/images/1.jpg", "assets/images/gulugod.png", "assets/images/2.jpg", "assets/images/4.jpg", "assets/images/6.jpg"];
 let slider = document.querySelector('.background-image');
 let sliderGridItems = [...document.querySelectorAll('.grid-item')];
 let currentImage = 0;
